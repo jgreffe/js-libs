14252068b29c8ff8e451a4f0398107c2b0d9ba42
jgreffe <jgreffe@cloudbees.com>
2022-07-28 16:58:12 +0200

    Update the deprecated/deleted methods from @jenkins-cd/js-modules

--- handlebars/gulpfile.js
+++ handlebars/gulpfile.js
@@ -4,5 +4,4 @@ const builder = require('@jenkins-cd/js-builder');
 // Bundle the module.
 //
 builder.bundle('handlebars', 'handlebars4')
-    .asJenkinsModuleResource()
     .export();
--- handlebars/src/test/js/handlebars-spec.js
+++ handlebars/src/test/js/handlebars-spec.js
@@ -10,8 +10,8 @@ describe("handlebars4.js", function () {
     it("- test", function (done) {
       jsTest.onPage(() => {
         const jenkins = require('@jenkins-cd/js-modules');
-        jenkins.export('handlebars', 'handlebars4', require('handlebars'));
-        jenkins.import("handlebars:handlebars4")
+        jenkins.exportModule('handlebars', 'handlebars4', require('handlebars'));
+        jenkins.importModule("handlebars:handlebars4")
             .onFulfilled(function(handlebars4) {
               expect(handlebars4).toBeDefined();
               done();
